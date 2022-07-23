49544e581feaa200657317d029f4bfad50b456b1
n3-rd <godwinjdayo@gmail.com>
2022-07-24 00:37:51 +0100

    fixed bot launch

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Welcome To Best In Comrade \n Send me a caption like and I'll generate a meme for you"))
+bot.start((ctx) =>
+  ctx.reply(
+    "Welcome To Best In Comrade \n Send me a caption like and I'll generate a meme for you"
+  )
+);
+
+bot.launch();
