394d9ee63d8b4be63adadf14b284cd8a74aa94aa
n3-rd <godwinjdayo@gmail.com>
2022-07-23 23:51:32 +0100

    added start functionality

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.start((ctx) => ctx.reply("Welcome To Best In Comrade \n Send me a caption like and I'll generate a meme for you"))
