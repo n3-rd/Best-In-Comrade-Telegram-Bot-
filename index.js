d50f55c4d8f329afa60588dbe5e957318bd2f192
n3-rd <godwinjdayo@gmail.com>
2022-07-24 13:26:49 +0100

    fixed cool meme for text users

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
 
 const { Telegraf } = require("telegraf");
-const express = require('express');
+const express = require("express");
 const expressApp = express();
 const axios = require("axios");
 const { uid } = require("uid");
 
-const API_TOKEN = process.env.API_TOKEN || '';
+const API_TOKEN = process.env.API_TOKEN || "";
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://best-in-bot.herokuapp.com';
-
+const URL = process.env.URL || "https://best-in-bot.herokuapp.com";
 
 const bot = new Telegraf(API_TOKEN);
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
-
 
 bot.start((ctx) =>
   ctx.reply(
@@ -22,9 +20,12 @@ bot.start((ctx) =>
   )
 );
 
-bot.on("text", (ctx)=>{
-  ctx.replyWithPhoto({url: `https://imgflip.com/i/6nqwiv`});
-  ctx.reply("Mention me @bestincomradebot alongside your caption in your chats to generate a meme");
+bot.on("text", (ctx) => {
+  // reply with meme
+  ctx.replyWithPhoto("https://i.imgflip.com/6nqwiv.jpg");
+  ctx.reply(
+    "Mention me @bestincomradebot alongside your caption in your chats to generate a meme"
+  );
 });
 
 bot.command("help", (ctx) =>
@@ -48,17 +49,16 @@ bot.on("inline_query", async (ctx) => {
 
   async function getMeme() {
     try {
-        if(query.length > 2) {
-
-        
-        const response = await axios.get(
+      if (query.length > 2) {
+        const response = await axios
+          .get(
             `https://api.imgflip.com/caption_image?template_id=393371323&username=${process.env.IMGFLIP_USERNAME}&password=${process.env.IMGFLIP_PASS}&text1=${query}`
-          ).then((response) => {
+          )
+          .then((response) => {
             console.log(response);
-    setTimeout(function() {
-
-    //         // response.data.data.url
-            ctx.answerInlineQuery([
+            setTimeout(function () {
+              //         // response.data.data.url
+              ctx.answerInlineQuery([
                 {
                   type: "photo",
                   id: "1",
@@ -67,31 +67,23 @@ bot.on("inline_query", async (ctx) => {
                   caption: query,
                   title: "genrated with @bestincomradebot",
                   photo_file_id: uid(20),
-          
                 },
               ]);
-    }, 2000);
-
-            });
-
-        }
-            // return photoUrl;
+            }, 2000);
+          });
+      }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-    }
+  }
 
-    getMeme();
-
-
-    
+  getMeme();
 });
 bot.launch();
 
-
-expressApp.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  expressApp.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+expressApp.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+expressApp.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
